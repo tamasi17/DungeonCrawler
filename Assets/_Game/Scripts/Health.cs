44ff5473df6ba44be8a3d6373441e68bc355ffcd
mats <mat.eidelman@gmail.com>
2026-01-25 22:19:22 +0100

    WIP: Health and Damage logics

---
+++ Assets/_Game/Scripts/Health.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+using UnityEngine.Events; // Needed for UnityEvents
+
+public class Health : MonoBehaviour, IDamageable
+{
+    [Header("Settings")]
+    [SerializeField] private int maxHealth = 100;
+    private int currentHealth;
+
+    [Header("Events")]
+    // This allows us to drag-and-drop functions in the Inspector (like playing a sound or particle)
+    public UnityEvent OnTakeDamage;
+    public UnityEvent OnDie;
+
+    private void Start()
+    {
+        currentHealth = maxHealth;
+    }
+
+    public void TakeDamage(int amount)
+    {
+        currentHealth -= amount;
+        Debug.Log($"{gameObject.name} took {amount} damage! HP: {currentHealth}");
+
+        // Trigger the "Ouch" event (flashing red, sound, etc.)
+        OnTakeDamage?.Invoke();
+
+        if (currentHealth <= 0)
+        {
+            Die();
+        }
+    }
+
+    private void Die()
+    {
+        Debug.Log($"{gameObject.name} has died.");
+        OnDie?.Invoke();
+    }
+}
